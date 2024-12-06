885e6c0e95e4689a98c5ffc81c979cb1132c1a86
PatrickNgare <patrickngare@gmail.com>
2024-12-06 15:49:24 +0300

    added the comments table fields

--- app/Models/post/Comment.php
+++ app/Models/post/Comment.php
@@ -6,5 +6,20 @@ use Illuminate\Database\Eloquent\Model;
 
 class Comment extends Model
 {
-    //
+
+   protected $table ='comments';
+
+    protected $fillable=[
+
+             'id',
+             'comment',
+             'user_id',
+             'user_name',
+             'post_id',
+             'created_at',
+
+
+
+
+    ];
 }
