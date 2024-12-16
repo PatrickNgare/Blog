0a2fa15c8403b70f056efa153bf28431b73f1ef2
PatrickNgare <patrickngare@gmail.com>
2024-12-16 10:03:25 +0300

    added the image ui in create post page

--- resources/views/posts/create-post.blade.php
+++ resources/views/posts/create-post.blade.php
@@ -3,25 +3,51 @@
 
 @section('content')
 
-<div class="pt-5 comment-form-wrap">
-    <h3 class="mb-5">Create a post</h3>
-    <form action="{{ route('comment.store') }}" method ="POST" class="p-5 bg-light">
+<div class="container">
 
-      @csrf
-      <div class="form-group">
+    <div class="pt-5 comment-form-wrap">
+        <h3 class="mb-5">Create a New Post</h3>
+        <form action="{{ route('comment.store') }}" method ="POST" class="p-5 bg-light">
 
-        <input type="hidden" name="post_id" value="" class="form-control" id="name">
+          @csrf
+          <div class="form-group">
+            <label for="title">Title</label>
+            <input type="text" placeholder="title" name="title" value="" class="form-control" id="name">
+          </div>
+
+           <div class="form-group">
+
+            <select name="category" class="form-select" aria-label="Default select example">
+                <option selected>Categories</option>
+                <option value="1">One</option>
+                <option value="2">Two</option>
+                <option value="3">Three</option>
+              </select>
+           </div>
+           <div class="form-group">
+            <input type="text" placeholder="user_id" name="user_id" value="" class="form-control" id="name">
+          </div>
+          <div class="form-group">
+            <input type="text" placeholder="user_name" name="user_name" value="" class="form-control" id="name">
+          </div>
+
+          <div class="form-group">
+            <label for="title">Image</label>
+            <input type="file"  name="image" value="" class="form-control" id="name">
+          </div>
+
+
+          <div class="form-group">
+            <label for="message">Description</label>
+            <textarea placeholder="Description" name="description"  cols="30" rows="10" class="form-control"></textarea>
+          </div>
+          <div class="form-group">
+            <input type="submit" name="submit" value="Post Comment" class="btn btn-primary">
+          </div>
+
+        </form>
       </div>
+</div>
 
 
-      <div class="form-group">
-        <label for="message">Post</label>
-        <textarea placeholder="comment" name="comment" id="message" cols="30" rows="10" class="form-control"></textarea>
-      </div>
-      <div class="form-group">
-        <input type="submit" name="submit" value="Post Comment" class="btn btn-primary">
-      </div>
-
-    </form>
-  </div>
   @endsection
