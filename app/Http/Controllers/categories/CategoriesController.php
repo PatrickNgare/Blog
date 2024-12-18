921b4417bbf3347ee19de185bb19094ceb0cb37b
PatrickNgare <patrickngare@gmail.com>
2024-12-18 20:49:01 +0300

    added te categoty function

--- app/Http/Controllers/categories/CategoriesController.php
+++ app/Http/Controllers/categories/CategoriesController.php
@@ -4,8 +4,18 @@ namespace App\Http\Controllers\categories;
 
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
+use App\Models\post\PostModel;
+
 
 class CategoriesController extends Controller
 {
-    //
+
+
+   public function category($name){
+
+  $post=PostModel::where('category',$name)->take(5)->orderBy('created_at', 'desc')->get();
+
+  return view('categories.category',compact('post'));
+   }
+
 }
