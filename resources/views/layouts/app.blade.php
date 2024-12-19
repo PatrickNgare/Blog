3c97b1ae554bed597351c956e7d20ee14dc5df27
PatrickNgare <patrickngare@gmail.com>
2024-12-19 11:45:49 +0300

    added the update profile view

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -87,13 +87,17 @@
     <li class="nav-item dropdown">
         <a id="navbarDropdown" class="nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown" aria-haspopup="true" aria-expanded="false" v-pre>
             {{ Auth::user()->name }}
+
         </a>
 
-        <a id="navbarDropdown" class="nav-link dropdown-toggle" href="{{ route('users.update',Auth::user()->id)  }}" role="button" data-bs-toggle="dropdown" aria-haspopup="true" aria-expanded="false" v-pre>
-            Update Profile
-        </a>
+
+
+
 
         <div class="dropdown-menu dropdown-menu-end" aria-labelledby="navbarDropdown">
+            <a class="dropdown-item" href="{{ route('users.update',Auth::user()->id)  }}">
+                Update Profile
+            </a>
             <a class="dropdown-item" href="{{ route('logout') }}"
                onclick="event.preventDefault();
                          document.getElementById('logout-form').submit();">
---
+++ resources/views/users/update-profile.blade.php
@@ -0,0 +1,33 @@
+
+@extends('layouts.app')
+
+@section('content')
+
+<div class="container">
+
+    <div class="pt-5 comment-form-wrap">
+        <h3 class="mb-5">Update Profile Info </h3>
+        <form action="{{ route('posts.update',$user->id)  }}" method ="POST" class="p-5 bg-light" enctype="multipart/form-data">
+
+          @csrf
+          <div class="form-group">
+            <label for="title">Email</label>
+            <input type="text" placeholder="email" name="email" value="{{ $user->email }}" class="form-control" id="name">
+          </div>
+          <div class="form-group">
+            <label for="message">Bio</label>
+            <textarea  placeholder="bio" name="bio"  cols="30" rows="10" class="form-control">{{ $user->bio }}</textarea>
+          </div><div class="form-group">
+            <label for="message">Name</label>
+            <textarea  placeholder="bio" name="name"  cols="30" rows="10" class="form-control">{{ $user->name }}</textarea>
+          </div>
+          <div class="form-group">
+            <input type="submit" name="submit" value="Update Profile" class="btn btn-primary">
+          </div>
+
+        </form>
+      </div>
+</div>
+
+
+  @endsection
