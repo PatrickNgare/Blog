c0da079cbd3c37e4b6146f9730bec5b4b74a077d
PatrickNgare <patrickngare@gmail.com>
2024-12-19 08:43:00 +0300

    added the category classification

--- app/Http/Controllers/categories/CategoriesController.php
+++ app/Http/Controllers/categories/CategoriesController.php
@@ -5,6 +5,7 @@ namespace App\Http\Controllers\categories;
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
 use App\Models\post\PostModel;
+use Illuminate\Support\Facades\DB;
 
 
 class CategoriesController extends Controller
@@ -14,8 +15,14 @@ class CategoriesController extends Controller
    public function category($name){
 
   $posts=PostModel::where('category',$name)->take(5)->orderBy('created_at', 'desc')->get();
+  $pupPosts=PostModel::take(3)->orderBy('id','desc')->get();
+  $categories = DB::table('categories')
+               ->join('posts', 'posts.user_id', '=', 'categories.id')
+               ->select('categories.name AS name', 'categories.id AS id', 'posts.user_id AS user_id', DB::raw('COUNT(posts.user_id) AS total'))
+               ->groupBy('categories.name', 'categories.id', 'posts.user_id')
+               ->get();
 
-  return view('categories.category',compact('posts'));
+  return view('categories.category',compact('posts','pupPosts','categories','name'));
    }
 
 }
--- resources/views/categories/category.blade.php
+++ resources/views/categories/category.blade.php
@@ -7,7 +7,7 @@
     <div class="container">
         <div class="row">
             <div class="col-12">
-                <div class="heading">Category: Business</div>
+                <div class="heading">Category: {{$name}}</div>
             </div>
         </div>
         <div class="row posts-entry">
@@ -22,7 +22,7 @@
                         <span class="date">{{ $post->created_at->format('M d, Y')}}&bullet; <a href="#">{{$post->category}}</a></span>
                         <h2><a href="{{route('posts.single',$post->id)}}">{{ $post->title }}</a></h2>
                         <p>{{ substr($post->description,0,300)  }}</p>
-                        <p><a href="single.html" class="btn btn-sm btn-outline-primary">Read More</a></p>
+                        <p><a href="{{ $post->created_at->format('M d, Y')}}" class="btn btn-sm btn-outline-primary">Read More</a></p>
                     </div>
                 </div>
                 @endforeach
@@ -34,56 +34,38 @@
 
 
                 <!-- END sidebar-box -->
-                <div class="sidebar-box">
+                <div class="sidebar-box"  style="padding: 30px">
                     <h3 class="heading">Popular Posts</h3>
                     <div class="post-entry-sidebar">
                         <ul>
-                            <li>
+                            @foreach ($pupPosts as $posts )
+                          <li>
                                 <a href="">
-                                    <img src="images/img_1_sq.jpg" alt="Image placeholder" class="rounded me-4">
+                                    <img src="{{ asset('assets/images/'.$post->image. '') }}" alt="Image placeholder" class="rounded me-4">
                                     <div class="text">
-                                        <h4>There’s a Cool New Way for Men to Wear Socks and Sandals</h4>
+                                        <h4>{{ $post->title }}</h4>
                                         <div class="post-meta">
-                                            <span class="mr-2">March 15, 2018 </span>
+                                            <span class="mr-2">{{ $post->created_at->format('M d, Y')}} </span>
                                         </div>
                                     </div>
                                 </a>
                             </li>
-                            <li>
-                                <a href="">
-                                    <img src="images/img_2_sq.jpg" alt="Image placeholder" class="rounded me-4">
-                                    <div class="text">
-                                        <h4>There’s a Cool New Way for Men to Wear Socks and Sandals</h4>
-                                        <div class="post-meta">
-                                            <span class="mr-2">March 15, 2018 </span>
-                                        </div>
-                                    </div>
-                                </a>
-                            </li>
-                            <li>
-                                <a href="">
-                                    <img src="images/img_3_sq.jpg" alt="Image placeholder" class="rounded me-4">
-                                    <div class="text">
-                                        <h4>There’s a Cool New Way for Men to Wear Socks and Sandals</h4>
-                                        <div class="post-meta">
-                                            <span class="mr-2">March 15, 2018 </span>
-                                        </div>
-                                    </div>
-                                </a>
-                            </li>
+                            @endforeach
+
                         </ul>
                     </div>
                 </div>
                 <!-- END sidebar-box -->
 
-                <div class="sidebar-box">
+                <div style="padding: 30px" class="sidebar-box">
                     <h3 class="heading">Categories</h3>
                     <ul class="categories">
-                        <li><a href="#">Food <span>(12)</span></a></li>
-                        <li><a href="#">Travel <span>(22)</span></a></li>
-                        <li><a href="#">Lifestyle <span>(37)</span></a></li>
-                        <li><a href="#">Business <span>(42)</span></a></li>
-                        <li><a href="#">Adventure <span>(14)</span></a></li>
+
+                        @foreach ( $categories as $category )
+                        <li><a href="{{ route('category.single',$category->name)  }}">{{ $category->name  }} <span>({{ $category->total  }})</span></a></li>
+                        @endforeach
+
+
                     </ul>
                 </div>
                 <!-- END sidebar-box -->
