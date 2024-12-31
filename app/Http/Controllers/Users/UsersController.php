02bdca5faae27cfac3b7e4e0d917be2b5476add6
PatrickNgare <patrickngare@gmail.com>
2024-12-31 09:37:40 +0300

    added the public profile

--- app/Http/Controllers/Users/UsersController.php
+++ app/Http/Controllers/Users/UsersController.php
@@ -52,5 +52,8 @@ public function profile($id){
 
 }
 
+
+
+
 }
 
--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -98,6 +98,9 @@
             <a class="dropdown-item" href="{{ route('users.edit',Auth::user()->id)  }}">
                 Update Profile
             </a>
+            <a class="dropdown-item" href="{{ route('users.edit',Auth::user()->id)  }}">
+                Update Profile
+            </a>
             <a class="dropdown-item" href="{{ route('logout') }}"
                onclick="event.preventDefault();
                          document.getElementById('logout-form').submit();">
